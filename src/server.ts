f2762963bdf568a2db184f95c4b38562e1fc3014
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-04-04 01:16:15 +0200

    feat: protect all the routes with a token

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express, { Express, Request, Response } from 'express';
 import morgan from 'morgan';
 import router from './router';
-import cors from 'cors'
+import cors from 'cors';
+import { protect } from './utils/auth';
 
 const app: Express = express();
 
-app.use(cors())
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +16,6 @@ app.get('/', (_req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello' });
 });
 
-app.use('/api', router);
+app.use('/api', protect, router);
 
 export default app;
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { User } from '@prisma/client';
+import { Request, Response, NextFunction } from 'express';
+
+type AuthenticatedRequest = Request & { user?: User };
 
 export const createJWT = (user: User) => {
   const token = jwt.sign(
@@ -11,4 +14,40 @@ export const createJWT = (user: User) => {
   );
 
   return token;
+};
+
+export const protect = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const bearer = req.headers.authorization;
+
+  if (!bearer) {
+    res.status(401).json({
+      status: 'error',
+      message: 'Not authorized',
+    });
+    return;
+  }
+
+  const [, token] = bearer.split(' ');
+  if (!token) {
+    res.status(401).json({
+      status: 'error',
+      message: 'Not authorized',
+    });
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.user = payload as User;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      status: 'error',
+      message: 'Invalid token',
+    });
+    return;
+  }
 };
